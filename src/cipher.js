a15abd3c1f40ec627061fb06f26b9933171f8245
JoannaAnaiz <joanna.yucra@gmail.com>
2019-03-05 09:44:10 -0500

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,22 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+  encode: (entrada1,offsetNumber1) => {
+    let codificar = "";
+    for(let i=0; i<entrada1.length; i++){ 
+      let ascii1 = entrada1.charCodeAt(i);
+      //if(ascii1>=65 && ascii1<=90){
+        codificar += String.fromCharCode((ascii1-65+offsetNumber1)%26+65);
+       // }
+       }
+       return codificar;
+    },
+  decode: (entrada2,offsetNumber2) => {
+    let decodificar = "";
+    for (let j=0; j<entrada2.length; j++){ 
+      let ascii2 = entrada2.charCodeAt(j);
+      //if(ascii2>=65 && ascii2<=90){
+        decodificar += String.fromCharCode((ascii2-offsetNumber2+65)%26+65);
+        //}
+        }
+        return decodificar;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-/* Acá va tu código */
+let encriptar = document.getElementById("encriptar");
+let desencriptar = document.getElementById("desencriptar");
+/*let desplazamiento = document.getElementById("desplazamiento").value;
+let entrada1 = document.getElementById("entrada1").value;
+let entrada2 = document.getElementById("entrada2").value;*/
+//let resultado = document.getElementById("resultado");
+
+
+encriptar.addEventListener('click', () =>{
+    let desplazamiento= document.getElementById("desplazamiento").value;
+    let offsetNumber1= parseInt(desplazamiento,10);
+    let entrada1=document.getElementById("entrada1").value;
+    document.getElementById("resultado").value=cipher.encode(entrada1,offsetNumber1);
+})
+
+desencriptar.addEventListener('click', () =>{
+    let desplazamiento= document.getElementById("desplazamiento").value;
+    let offsetNumber2= parseInt(desplazamiento,10);
+    let entrada2=document.getElementById("entrada2").value;
+    document.getElementById("resultado").value=cipher.decode(entrada2,offsetNumber2);
+})
